5d7a5fce732415f477eaa9164dc870b580d0f332
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-08-29 14:40:09 -0400

    fixed front-end files

--- client/src/Routes/App.js
+++ client/src/Routes/App.js
@@ -1,22 +1,26 @@
 import React, {useEffect, useState} from "react";
 import {Outlet, Link} from 'react-router-dom'
 import Navbar from '../Components/Navbar';
+import SignInPage from "./SigninButton";
+import Home from "./Home";
 
 
 export default function App () {
     
     const [searchInput, setSearchInput] = useState("")
 
-useEffect(() => {
-    fetch("url")
-    .then((response) => response.json())
-    .then((data => setSearchProducts(data)))
-})
+    // useEffect(() => {
+    //     fetch("url")
+    //     .then((response) => response.json())
+    //     .then((data => setSearchProducts(data)))
+    // })
     
     return ( 
         <div>
             <Navbar searchInput={searchInput} setSearchInput={setSearchInput}/>
                 <Outlet/>
+            <Home/>
+            <SignInPage />
         </div>
         
         
