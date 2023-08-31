11a5387dc9c5f653d7477222119ba7b169845846
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-08-31 09:50:40 -0400

    comments

--- server/combined-apis/models.py
+++ server/combined-apis/models.py
@@ -14,11 +14,11 @@ class User(db.Model):
     
     wishlists = db.relationship("Wishlist", back_populates="user")
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    # def set_password(self, password):
+    #     self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    # def check_password(self, password):
+    #     return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"<User {self.username}>"
