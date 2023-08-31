6e416823bfd4dc2f8ac066c8c4fd6acca49ff6ec
Alice Hepburn <aliceokhepburn@gmail.com>
2023-08-31 14:29:57 -0400

    Merge conflict resolution for frontend contributions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {Outlet, Link} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Outlet, Link } from 'react-router-dom'
 import Navbar from './Components/Static/Navbar';
 import CreateAccountPage from './Routes/CreateAccountPage';
 import { Routes, Route } from 'react-router-dom';
@@ -9,18 +9,18 @@ import Products from "./Components/Pieces/Products";
 import FeaturedProduct from "./Components/Pieces/FeaturedProduct";
 
 
-// The main app page, pareant. landing page, idk // 
+// The main app page, pareant. landing page, idk //
 
-export default function App () {
+export default function App() {
 
     const [currentUser, setCurrentUser] = useState(null)
     const [searchInput, setSearchInput] = useState("")
     const [products, setProductsList] = useState([])
     const [wishlist, setWishlist] = useState([])
-    const [featuredProduct, setFeaturedProduct] = useState ([])
+    const [featuredProduct, setFeaturedProduct] = useState([])
 
-//PRODUCT SEARCH
-    function handleSearch(userentry){
+    //PRODUCT SEARCH
+    function handleSearch(userentry) {
         setSearchInput(userentry)
 
         fetch('/search', {
@@ -29,20 +29,20 @@ export default function App () {
                 'Content-Type': 'application/json',
                 'Accepts': 'application/json'
             },
-            body: JSON.stringify({userentry})
-        }) 
-        .then(response => response.json())
-        .then(data => setProductsList(data));
+            body: JSON.stringify({ userentry })
+        })
+            .then(response => response.json())
+            .then(data => setProductsList(data));
     }
 
-// curl -X POST -H "Content-Type: application/json" -d '{ "query": "shirt"}' localhost:5555/search
+    // curl -X POST -H "Content-Type: application/json" -d '{ "query": "shirt"}' localhost:5555/search
 
-//FEATURED PRODUCT 
+    //FEATURED PRODUCT 
     function handleFeaturedProduct(clickedProduct) {
         setFeaturedProduct(clickedProduct)
-}
+    }
 
-//WISHLIST 
+    //WISHLIST 
     function handleAddToWishlist(productToAdd) {
         // fetch ("/wishlist", {
         //     method: "POST",
@@ -52,19 +52,19 @@ export default function App () {
         //     body: JSON.stringify(productToAdd),
         // });
 
-            setWishlist([...wishlist, productToAdd])
-        }
+        setWishlist([...wishlist, productToAdd])
+    }
 
-//LOGING + SIGNUP 
+    //LOGING + SIGNUP 
     useEffect(() => {
         fetch('/check_session')
-        .then(res => {
-            res.json()
-            .then(user => setCurrentUser(user))
-        })
+            .then(res => {
+                res.json()
+                    .then(user => setCurrentUser(user))
+            })
     }, [])
 
-    function createAccount(userInfo){
+    function createAccount(userInfo) {
         fetch('/users', {
             method: 'POST',
             headers: {
@@ -72,11 +72,12 @@ export default function App () {
                 'Accepts': 'application/json'
             },
             body: JSON.stringify(userInfo)
-        }) 
-        .then(response => response.json())
-        .then(data => setCurrentUser(data))};
+        })
+            .then(response => response.json())
+            .then(data => setCurrentUser(data))
+    };
 
-    function attemptLogin(userInfo){
+    function attemptLogin(userInfo) {
         fetch('/login', {
             method: 'POST',
             headers: {
@@ -84,30 +85,31 @@ export default function App () {
                 'Accepts': 'application/json'
             },
             body: JSON.stringify(userInfo)
-        }) 
-        .then(response => response.json())
-        .then(data => setCurrentUser(data))};
-
-    function logout(){
-        fetch('/logout', {method: 'DELETE'})
-        .then(res => {
-            if(res.ok){
-                setCurrentUser(null)
-            }
         })
+            .then(response => response.json())
+            .then(data => setCurrentUser(data))
+    };
+
+    function logout() {
+        fetch('/logout', { method: 'DELETE' })
+            .then(res => {
+                if (res.ok) {
+                    setCurrentUser(null)
+                }
+            })
     }
     console.log(handleAddToWishlist)
 
-    return ( 
+    return (
         <div>
-            <Navbar searchInput={searchInput} handleSearch={handleSearch}/> 
-            <Products searchInput={searchInput} products={products} handleAddToWishlist={handleAddToWishlist} handleFeaturedProduct= {handleFeaturedProduct}/> 
-            <FeaturedProduct featuredProduct={featuredProduct} handleAddToWishlist={handleAddToWishlist}/>
-            <CreateAccountPage createAccount={createAccount}/> 
-            <LoginPage attemptLogin={attemptLogin} /> 
+            <Navbar searchInput={searchInput} handleSearch={handleSearch} />
+            <Products searchInput={searchInput} products={products} handleAddToWishlist={handleAddToWishlist} handleFeaturedProduct={handleFeaturedProduct} />
+            <FeaturedProduct featuredProduct={featuredProduct} handleAddToWishlist={handleAddToWishlist} />
+            <CreateAccountPage createAccount={createAccount} />
+            <LoginPage attemptLogin={attemptLogin} />
             <WishlistProducts setWishlist={setWishlist} wishlist={wishlist} />
         </div>
-        
-        
-        );
-    }
+
+
+    );
+}
