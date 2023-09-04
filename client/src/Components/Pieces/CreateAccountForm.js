18deac3cc96fedc5943fca93ad490a262953a4c7
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-09-04 12:45:56 -0400

    Create Account v1

--- client/src/Components/Pieces/CreateAccountForm.js
+++ client/src/Components/Pieces/CreateAccountForm.js
@@ -16,10 +16,30 @@ export default function CreateAccountForm({
     return (
         <div className="container">
             <div className="left">
-                <h1>Create your account</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
+                <h1>Create an account</h1>
+                <p> Be sure to enter the correct information</p>
+                <p>Required fields have an asterik: *</p>
+                <form>
+                    <h2>Legal first name*</h2>
+                    <input></input>
+                    <h2>Legal last name*</h2>
+                    <input></input>
+                    <h2>Email*</h2>
+                    <input></input>
+                    <h2>Password*</h2>
+                    <input></input>
+                    <br></br>
+                    <label for="ageCertification">
+                        <input type="checkbox" id="ageCertification" name="ageCertification" required />
+                        I certify that I am 18 years of age or older, I agree to the User Agreement, and I have read the Privacy Policy.
+                    </label>
+                    <button>Create free account</button>
+                </form>
+                {/* Add social media sign-up options */}
+                <div className="social-media-signup">
+                    <button className="social-media-button">Sign up with Google</button>
+                    <button className="social-media-button">Sign up with Facebook</button>
+                </div>
             </div>
             <div className="right">
                 <form className="form-container" onSubmit={handleSubmit}>
--- client/src/Components/Routes/Welcome.js
+++ client/src/Components/Routes/Welcome.js
@@ -5,21 +5,6 @@ import { Link } from 'react-router-dom'
 
 export default function Welcome() {
     return (
-        <div className="welcome-container">
-            <video className="bg-video" autoPlay loop muted>
-                <source src={bgVideo} type="video/mp4" />
-            </video>
-            <div className="content">
-                <h1>Welcome to Thrifty! 🛒 </h1>
-                <h2>Tired of tab-hopping? We got you! Thrifty is your one-stop shop to compare deals and snag the best bargains. We pull the coolest stuff from eBay and Poshmark and lay it all out for you. No more FOMO, only smart shopping. Dive in and let your wallet thank you! 💰</h2>
-                <br></br>
-                <p>click this button below to find out more</p>
-                <br></br>
-                {/* <a><button href=""> SHOP </button></a> */}
-                <Link to='/products'><button >SHOP</button></Link>
-
-            </div>
-
-        </div>
+        <></>
     );
 }
